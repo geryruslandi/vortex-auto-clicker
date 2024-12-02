fcb2447575d04443ea356546b90c510500c73aa3
Gery Ruslandi <geryruslandi@gmail.com>
2024-12-02 10:41:44 +0700

    refactor the code structure, and add additional functionality for Cyberpunk game

--- auto_downloader.py
+++ auto_downloader.py
@@ -1,44 +1,78 @@
 import os
 import pyautogui
 import time
+from pyscreeze import ImageGrab
+import traceback
 
 print(os.getcwd())
 
 pyautogui.FAILSAFE = False
 
 # Image file paths
-launcher_download_path = "download_launcher.PNG"
-website_download_path = "download_website.PNG"
+launcher_download_path = "./images/download_launcher.PNG"
+website_download_path = "./images/download_website.PNG"
 threshold = 0.9
+
+def clickOnImage(location: str, screenshot) -> bool:
+    try:
+        ui_location = pyautogui.locate(location, screenshot, confidence=threshold)
+        print(ui_location)
+        if ui_location is not None:
+            pyautogui.click(ui_location.left + 5, ui_location.top + 5)
+            return True
+        return False
+    except Exception as e:
+        return False
+
+def isCyberpunkGame(screenshot) -> bool:
+  try:
+    cyberpunk_path = "./images/cyberpunk/cyberpunk_identifier.png"
+    ui_location = pyautogui.locate(cyberpunk_path, screenshot, confidence=threshold)
+    print("Is cyberpunk game, checking additional cyberpunk button as well")
+    return ui_location is not None
+  except:
+    return False
+
+def checkOnCommonButtons(screenshot) -> bool:
+  if clickOnImage(launcher_download_path, screenshot):
+    print("Clicked on 'Launcher Download'")
+    return True
+  elif clickOnImage(website_download_path, screenshot):
+    print("Clicked on 'Website Download'")
+    return True
+  return False
+
+def checkOnCyberpunkButtons(screenshot) -> bool:
+  install_staging_path = "./images/cyberpunk/install_staging.png"
+  understood_path = "./images/cyberpunk/understood.png"
+  continue_path = "./images/cyberpunk/continue.png"
+
+  if clickOnImage(understood_path, screenshot):
+    print("Clicked on 'Understood'")
+    return True
+  elif clickOnImage(continue_path, screenshot):
+    print("Clicked on 'Install Staging'")
+    return True
+  elif clickOnImage(install_staging_path, screenshot):
+    print("Clicked on 'Install Staging'")
+    return True
+  return False
 
 while True:
     try:
-        print("Checking for Launcher Download Button")
-        launcher_location = pyautogui.locateOnScreen(launcher_download_path, confidence=threshold)
+        print("Checking for buttons...")
+        screenshot = ImageGrab.grab(all_screens=True)
 
-        if launcher_location is not None:
-            # Click on the launcher download button if found
-            pyautogui.click(launcher_location.left + 5, launcher_location.top + 5)
+        if checkOnCommonButtons(screenshot):
+          time.sleep(2)
+          break
 
-        # Introduce a delay before taking the screenshot
-        time.sleep(3)
-    
-    except pyautogui.ImageNotFoundException:
-        # Handle the exception (e.g., print a message)
-        print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+        if isCyberpunkGame(screenshot):
+          checkOnCyberpunkButtons(screenshot);
 
-    try:
-        print("Checking for Website Download Button")
-        website_location = pyautogui.locateOnScreen(website_download_path, confidence=threshold)
 
-        if website_location is not None:
-            # Click on the website download button if found
-            pyautogui.click(website_location.left + 5, website_location.top + 5)
+    except Exception as e:
+      traceback.print_exc()
+      print(f"Error in main loop: {e}")
 
-        time.sleep(3)
-        print("Waiting for 5 seconds...")
-    except pyautogui.ImageNotFoundException:
-        # Handle the exception (e.g., print a message)
-        print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+    time.sleep(2)
